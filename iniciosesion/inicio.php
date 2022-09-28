57371c9d07be28a16f24b4c816b7cd36da05f5cd
tomaskehl3 <106130890+tomaskehl3@users.noreply.github.com>
2022-09-28 08:17:42 -0300

    nuevo

--- iniciosesion/inicio.php
+++ iniciosesion/inicio.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda de ropa</title>
-    <link rel="stylesheet" href="programacion.php">
-    <link rel="stylesheet" href="estilos.css">
+    <title>Document</title>
 </head>
 <body>
-    
+<?php
+
+
+
+
+?>
 </body>
 </html>
