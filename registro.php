0ecead2ec9b7385ceb9944643f9ad87ea94e3866
tomaskehl3 <106130890+tomaskehl3@users.noreply.github.com>
2022-09-28 09:30:08 -0300

    nuevo3

---
+++ registro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$conexion=mysqli_connect("localhost","root","","tiendaropa") or
+die("Problemas con la conexión");
+
+mysqli_query($conexion,"insert into iniciosesion(nombre,mail,contraseña) values 
+('$_REQUEST[nombre1]','$_REQUEST[mail1]','$_REQUEST[contraseña1]')")
+or die("Problemas en el select".mysqli_error($conexion));
+
+mysqli_close($conexion);
+
+echo "registrado correctamente.";
+
+
+
+?>
+</body>
+</html>
