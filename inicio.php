e0aa6b946931df7f37b86df179cb0106244e03e1
tomaskehl3 <106130890+tomaskehl3@users.noreply.github.com>
2022-11-06 19:55:10 -0300

    6del11
    login hecho

--- inicio.php
+++ inicio.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,15 +8,40 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+$email=$_POST['mail'];
+$contraseña=$_POST['contraseña'];
+session_start();
+$_SESSION['mail']=$email;
 
-<!-- ACTION: inicio.php - METHOD: post
-NAME DEL MAIL: mail
-NAME DE LA CONTRASEÑA: contraseña -->
-<?php
+$conexion=mysqli_connect("localhost","root","","tiendaropa") or
+die("Problemas con la conexión");
 
+
+$consulta="SELECT*FROM iniciosesion where mail='$email' and contraseña='$contraseña'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+    header("location:lookbook.html");
+}
+else{
+    ?>
+    <?php
+    include("login.html");
+    ?>
+    
+    <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
 
 
 
 ?>
 </body>
 </html>
+
+
+
